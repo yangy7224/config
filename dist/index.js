160235dfebb01defc47efa2deb12f153145398b5
yangy <yangy@7224.com>
2019-03-04 13:52:36 +0800

    Update index.js
    修复打包错误

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,9 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
 /**
  * Created by yiyang1990 on 2019/1/4.
  */
 
-var interfaceConfig = function () {
-  function interfaceConfig() {
-    _classCallCheck(this, interfaceConfig);
-
+class interfaceConfig {
+  constructor(){
     this.env_dev_url = 'http://192.168.1.141:91';
     this.env_dev_proxy_url = '/api';
     this.env_production_url = 'https://ecportal.api.7224.com';
@@ -28,21 +16,14 @@ var interfaceConfig = function () {
     this.baseURL = process.client ? this.getCurrentConfigProxyUrl() : this.getCurrentConfigUrl();
   }
 
-  _createClass(interfaceConfig, [{
-    key: 'getCurrentConfigUrl',
-    value: function getCurrentConfigUrl() {
-      return process.env.NODE_ENV == "production" ? this.env_production_url : this.env_dev_url;
-    }
-  }, {
-    key: 'getCurrentConfigProxyUrl',
-    value: function getCurrentConfigProxyUrl() {
-      return process.env.NODE_ENV == "production" ? this.env_production_url : this.env_dev_proxy_url;
-    }
-  }]);
+  getCurrentConfigUrl(){
+   return process.env.NODE_ENV == "production" ? this.env_production_url : this.env_dev_url;
+  }
 
-  return interfaceConfig;
-}();
+  getCurrentConfigProxyUrl(){
+    return process.env.NODE_ENV == "production" ? this.env_production_url : this.env_dev_proxy_url;
+  }
 
-;
+};
 
-exports.default = new interfaceConfig();
+export default new interfaceConfig();
